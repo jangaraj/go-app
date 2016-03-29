7c33478293a38bbd8f2b00cc5815fbf07216ba8c
Jan Garaj <jan.garaj@gmail.com>
2016-03-29 18:54:40 +0100

    This will be only pure go-app code - no ops/build codes

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export GIT_REVISION=aaaaa
-export BUILD_DATE=$(date +%Y-%m-%d)
-export LD_FLAGS="-X main.gitRevision=${GIT_REVISION} -X main.buildDate=${BUILD_DATE}"
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -ti google/golang CGO_ENABLED=0 GOOS=linux go build -a -ldflags  
