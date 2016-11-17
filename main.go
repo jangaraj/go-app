e0cf0a1451c5dd1ee369691424caeb8461064d7c
Jan Garaj <info@monitoringartist.com>
2016-11-17 11:51:44 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
         h, _ := os.Hostname()
         fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+        for _, e := range os.Environ() {
+            pair := strings.Split(e, "=")
+                fmt.Println(pair[0],": ", pair[1])
+    }
 }
 func main() {
         http.HandleFunc("/", handler)
