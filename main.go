d7812b9ac69560ddbf3b02143dd4b54fcc62b231
Jan Garaj <info@monitoringartist.com>
2016-11-17 11:57:45 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ import (
         "fmt"
         "net/http"
         "os"
+        "string"
 )
 func handler(w http.ResponseWriter, r *http.Request) {
         h, _ := os.Hostname()
         fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
         for _, e := range os.Environ() {
             pair := strings.Split(e, "=")
-                fmt.Println(pair[0],": ", pair[1])
+            fmt.Println(pair[0],": ", pair[1])
     }
 }
 func main() {
